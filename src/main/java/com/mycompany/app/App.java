947ab164b67e465970a20bdc03fcf7f38298ce1f
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-10-18 19:18:08 +0530

    init

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public Integer add(Integer a, Integer b) {
+      return a + b;
+    }
+}
