078b133347ce3a643523b117c673a02952e44e6a
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-10-20 13:56:26 +0530

    increment

--- src/main/java/com/mycompany/products/Car.java
+++ src/main/java/com/mycompany/products/Car.java
@@ -7,12 +7,25 @@ package com.mycompany.products;
 public class Car implements Vehicle
 {
   private Integer position;
+  protected Integer wheelsCount;
 
   public Car() {
     this.position = 0;
+    this.wheelsCount = 0;
   }
 
   public void moveForward(Integer kilometer) {
     this.position += kilometer;
+  }
+
+  private class Seat {
+    Integer capacity;
+  }
+}
+
+class Bmw extends Car {
+  public Bmw() {
+    super();
+    super.wheelsCount = 4;
   }
 }
